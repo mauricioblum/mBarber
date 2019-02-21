fe603a7c9ab085dc5cc55518b50f0839aaf62e29
Mauricio Blum <mauriciocblum@gmail.com>
2019-02-21 01:15:42 -0300

    Added Routes to prevent login while logged using middlewares,

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -18,6 +18,8 @@ class SessionController {
       console.log('Senha incorreta')
       return res.redirect('/')
     }
+
+    req.session.user = user
     return res.redirect('/app/dashboard')
   }
 }
